0b58dbeb9bbf1fd709af55ac27c72d835834e4ac
vplacido <valentinplacido25@gmail.com>
2020-07-03 22:25:51 -0400

    added the relationships for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :saves #as a buyer
+    has_many :reviews # as a buyer
+    has_many :bookings #as a buyer
+
+    has_many :listings # as a realtor
+
 end
